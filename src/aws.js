d4abee1a381ec6bd7f0b1e4d1ef4b39a71f9b0ec
Maciej Górnicz <maciej.gornicz@miw.group>
2022-02-14 16:29:58 +0100

    fix root running problem

--- src/aws.js
+++ src/aws.js
@@ -12,7 +12,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
       'su - ubuntu',
       `cd "${config.input.runnerHomeDir}"`,
       `su - ubuntu -c 'cd "${config.input.runnerHomeDir}" && ./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}'`,
-      `su - ubuntu -c './run.sh'`,
+
     ];
   } else {
     return [
@@ -24,7 +24,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
       "su - ubuntu -c 'curl -O -L https://github.com/actions/runner/releases/download/v2.287.1/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz'",
       "su - ubuntu -c 'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz'",
       `su - ubuntu -c './config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}'`,
-      "su - ubuntu -c './run.sh'",
+
     ];
   }
 }
