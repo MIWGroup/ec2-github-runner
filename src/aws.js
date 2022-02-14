9effe3d7414004935b740f778db10156d8f772c4
Maciej Górnicz <maciej.gornicz@miw.group>
2022-02-14 16:53:52 +0100

    fix root running problem

--- src/aws.js
+++ src/aws.js
@@ -49,7 +49,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
   try {
     const result = await ec2.runInstances(params).promise();
     const ec2InstanceId = result.Instances[0].InstanceId;
-    core.info(`AWS EC2 instance ${ec2InstanceId} is started`);
+    core.info(`AWS EC2 instance ${ec2InstanceId} is started now`);
     return ec2InstanceId;
   } catch (error) {
     core.error('AWS EC2 instance starting error');
